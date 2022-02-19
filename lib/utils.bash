d4bc51fe5fbd6bd0d82b4258ec5878a534622deb
Gabor Szakacs <gabor@simp.hu>
2022-02-19 20:10:36 +0100

    Fixes

--- lib/utils.bash
+++ lib/utils.bash
@@ -58,8 +58,8 @@ install_version() {
   fi
 
   (
-    mkdir -p "$install_path"
-    cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+    mkdir -p "$install_path/bin"
+    cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path/bin"
 
     # TODO: Asert bitrise executable exists.
     local tool_cmd
